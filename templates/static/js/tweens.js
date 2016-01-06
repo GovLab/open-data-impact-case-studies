e411f6a6a7b6504cd92663e3969556f3dc9a35fd
mocxd <mark.adkins.hastings@gmail.com>
2016-01-06 02:55:02 -0500

    text changes main page

--- templates/static/js/tweens.js
+++ templates/static/js/tweens.js
@@ -4,4 +4,8 @@ $(document).ready(function(){
         TweenLite.to(window, 0.5, {scrollTo:$('#a-explore').offset().top, ease:Power2.easeOut});
     });
 
+    $('#top-section-arrow').click(function() {
+        TweenLite.to(window, 0.5, {scrollTo:$('#a-section-1').offset().top, ease:Power2.easeOut});
+    });
+
 });
