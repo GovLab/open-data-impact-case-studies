79a48353a609adf44cc982278f782f672cd8c3ed
mocxd <mark.adkins.hastings@gmail.com>
2016-01-06 00:18:28 -0500

    updates main page html

--- templates/static/js/tweens.js
+++ templates/static/js/tweens.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $('#nav-explore').click(function() {
-        TweenLite.to(window, 0.3, {scrollTo:$('#a-explore').offset().top, ease:Power2.easeOut});
+        TweenLite.to(window, 0.5, {scrollTo:$('#a-explore').offset().top, ease:Power2.easeOut});
     });
 
 });
