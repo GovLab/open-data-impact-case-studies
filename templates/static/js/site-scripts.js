34f2caf1860468d58fc718fcb69bb458373570f4
mocxd <mark.adkins.hastings@gmail.com>
2016-01-08 15:01:48 -0500

    fixes flicker by moving data-equalizer to parent tab, removes workaround js

--- templates/static/js/site-scripts.js
+++ templates/static/js/site-scripts.js
@@ -3,7 +3,7 @@
 $(function() {
     // reflow the equalizer on tab change
     $('#explore-tabs').on('change.zf.tabs', function() {
-        //$('#' + $(this).attr('id')).foundation();
-        $(window).resize();  // workaround due to foundation 6 reflow not working properly??
+        //$('#' + $(this).attr('id')).foundation(); // doesn't work, although this was supposed to be the way to 'reflow' stuff in foundation 6
+        // $(window).resize();  // workaround due to foundation 6 reflow not working properly??
     });
 });
