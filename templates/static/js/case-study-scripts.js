3c2c2c71e2aacfaa4d75cac6acc8ed41076e584a
mocxd <mark.adkins.hastings@gmail.com>
2016-01-14 02:15:04 -0500

    adds social sidebar

---
+++ templates/static/js/case-study-scripts.js
@@ -0,0 +1,26 @@
+$(function() {
+
+    $('.js-more-toggle').click(function() {
+        $(this).parent().toggleClass('m-active');
+    });
+
+    var sidebar = $('.b-sidebar');
+    var offset = sidebar.offset().top;
+
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > offset) {
+            if (sidebar.hasClass('js-scroll-none')) {
+                sidebar.removeClass('js-scroll-none');
+                sidebar.addClass('js-scroll-fixed');
+            }
+        }
+        else if (scroll < offset) {
+            if (sidebar.hasClass('js-scroll-fixed')) {
+                sidebar.removeClass('js-scroll-fixed');
+                sidebar.addClass('js-scroll-none');
+            }
+        }
+    });
+
+});
