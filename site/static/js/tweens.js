afa08a1322f3c36c5441596d76621229abbf9d72
mocxd <mark.adkins.hastings@gmail.com>
2016-01-15 12:11:59 -0500

    rebuild

--- site/static/js/tweens.js
+++ site/static/js/tweens.js
@@ -12,6 +12,10 @@ $(document).ready(function(){
         TweenLite.to(window, 0.5, {scrollTo:$('#a-section-1').offset().top, ease:Power2.easeOut});
     });
 
+    var trigger1 = $('#trigger-1');
+    var trigger2 = $('#trigger-2');
+    var trigger3 = $('#trigger-3');
+    var trigger4 = $('#trigger-4');
     var badge1 = $('#badge-1');
     var badge2 = $('#badge-2');
     var badge3 = $('#badge-3');
@@ -27,29 +31,29 @@ $(document).ready(function(){
 
         if (!badge1Done) {
             // if center of window is scrolled past the top of the element
-            if (scroll > badge1.offset().top-windowHeight/2) {
-                TweenLite.to(badge1, 1, {color:'rgba(0,136,149,.5)'});
+            if (scroll > trigger1.offset().top-windowHeight/2) {
+                TweenLite.to(badge1, 1, {opacity: 1});
                 badge1Done = true;
             }
         }
 
         if (!badge2Done) {
-            if (scroll > badge2.offset().top-windowHeight/2) {
-                TweenLite.to(badge2, 1, {color:'rgba(0,136,149,.5)'});
+            if (scroll > trigger2.offset().top-windowHeight/2) {
+                TweenLite.to(badge2, 1, {opacity: 1});
                 badge2Done = true;
             }
         }
 
         if (!badge3Done) {
-            if (scroll > badge3.offset().top-windowHeight/2) {
-                TweenLite.to(badge3, 1, {color:'rgba(0,136,149,.5)'}); // $teal
+            if (scroll > trigger3.offset().top-windowHeight/2) {
+                TweenLite.to(badge3, 1, {opacity: 1}); // $teal
                 badge3Done = true;
             }
         }
 
         if (!badge4Done) {
-            if (scroll > badge4.offset().top-windowHeight/2) {
-                TweenLite.to(badge4, 1, {color:'rgba(0,136,149,.5)'});
+            if (scroll > trigger4.offset().top-windowHeight/2) {
+                TweenLite.to(badge4, 1, {opacity: 1});
                 badge4Done = true;
             }
         }
