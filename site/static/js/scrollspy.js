c48664b1fcdf7a56d1b757d510cc1fa8a0595332
Denny Tek <tek.denny@gmail.com>
2016-03-21 11:43:48 -0400

    Bind nav links to scroll for proper placement on page

--- site/static/js/scrollspy.js
+++ site/static/js/scrollspy.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+// BIND CLICK TO NAVIGATION AND ANIMATE SCROLLING
+    // LOCATE THE NAV MENU
+    var scrollMenu = $(".e-scrollspy");
+    // GRAB ALL LINKS
+    var scrollMenuItems = scrollMenu.find("a");
+
+    scrollMenuItems.on ("click", function(e) {
+        e.preventDefault();
+        var link = $(this).attr("href");
+        var linkOffset =  $(link).offset().top - $(".e-nav-group").height();
+        // console.log("link", link);
+        // console.log("link offset", linkOffset);
+        $('html, body').stop().animate({
+            'scrollTop': linkOffset
+        }, 400);
+    })
+
+
 // STICKY NAVIGATION
     $( window ).scroll( function(){
         // SCROLL LOCATION
@@ -8,11 +26,14 @@ $(document).ready(function() {
         var visibleArea = $( window ).height();
         // WHERE TO STICK THE NAV USING ELEMENT HEIGHT
         var headerHeight = $( '.b-top-nav' ).height() + $(".e-nav-group").height();
-
+        // FIND SCROLL POSITION TO LOCK VIEW
         var maxScroll = visibleArea - headerHeight;
 
-
-    console.log(yPosition,maxScroll);
+        // console.log("yPosition", yPosition);
+        // console.log("visibleArea", visibleArea);
+        // console.log("headerHeight", headerHeight);
+        // console.log("maxScroll", maxScroll);
+        // console.log(yPosition,maxScroll);
         if ( yPosition > maxScroll) {
             $('.e-scrollspy').css('position', "fixed");
             $('.e-scrollspy').css('top',$(".e-nav-group").height());
@@ -21,8 +42,9 @@ $(document).ready(function() {
         } else {
             $('.e-scrollspy').css('position', "static");
             $('.long-reading').removeClass("medium-offset-3");
-            }
-        });
+        }    
+    });
+
 
 });
 
