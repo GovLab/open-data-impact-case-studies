bf7da23204c4ebad7d0c5e8524a2c82718cff2ec
Denny Tek <tek.denny@gmail.com>
2016-03-21 10:42:30 -0400

    Add initial sticky nav js

--- templates/static/js/scrollspy.js
+++ templates/static/js/scrollspy.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
 
+// STICKY NAVIGATION
+    $( window ).scroll( function(){
+        // SCROLL LOCATION
+        var yPosition = $( window ).scrollTop();
+        // HEIGHT OF WINDOW
+        var visibleArea = $( window ).height();
+        // WHERE TO STICK THE NAV USING ELEMENT HEIGHT
+        var headerHeight = $( '.b-top-nav' ).height() + $(".e-nav-group").height();
+
+        var maxScroll = visibleArea - headerHeight;
+
+
+    console.log(yPosition,maxScroll);
+        if ( yPosition > maxScroll) {
+            $('.e-scrollspy').css('position', "fixed");
+            $('.e-scrollspy').css('top',$(".e-nav-group").height());
+            $('.long-reading').addClass("medium-offset-3 end");
+            $('.b-top-nav').css('display','block');
+        } else {
+            $('.e-scrollspy').css('position', "static");
+            $('.long-reading').removeClass("medium-offset-3");
+            }
+        });
+
 });
+
+
+
+
+
+
+
+
+
