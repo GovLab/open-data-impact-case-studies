15314b37ce39c2c01a035cb0c1d83cbc562ab2fa
Denny Tek <tek.denny@gmail.com>
2016-03-23 04:24:53 -0400

    Add jquery and foundation versions of explore page

---
+++ templates/static/js/explore.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    $("#map-visualization").hide();
+
+    $('#explore-button-graphic').on ("click", function() {
+        $("#circle-graph").show();
+        $("#explore-tab-caption").html("<p>Explore how open data facilitates innovation by delving into areas of impact</p>");
+        $("#map-visualization").hide();
+    })
+
+    $('#explore-button-map').on ("click", function() {
+        $("#explore-tab-caption").html("<p>To understand how open data transforms our world, click on the icons in this interactive graphic to select a global region and then sort case studies either by sector or area of impact.</p>");
+        $("#map-visualization").toggle();
+        $("#circle-graph").hide();
+    })
+
+
+});
+
+
+
+
+
+
+
+
+
