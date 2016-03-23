e9beba54e94837e7eebe7e19e1c7e8d7e4c4ca81
Denny Tek <tek.denny@gmail.com>
2016-03-23 09:14:43 -0400

    Adjust styling issues on explore page

--- templates/static/js/explore.js
+++ templates/static/js/explore.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
     $('#explore-button-map').on ("click", function() {
         $("#explore-tab-caption").html("<p>To understand how open data transforms our world, click on the icons in this interactive graphic to select a global region and then sort case studies either by sector or area of impact.</p>");
-        $("#map-visualization").toggle();
+        $("#map-visualization").show();
         $("#circle-graph").hide();
     })
 
