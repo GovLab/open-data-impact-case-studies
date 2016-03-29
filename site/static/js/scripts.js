a776cfed115cf36cdd1ce35d34954e078e675394
mocxd <mark.adkins.hastings@gmail.com>
2016-03-29 12:50:04 -0400

    rebuild

--- site/static/js/scripts.js
+++ site/static/js/scripts.js
@@ -467,21 +467,21 @@ window.Feed({
 
     // http://stackoverflow.com/a/7768006
 
-    var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
-    var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
-    var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
-    var is_safari = navigator.userAgent.indexOf("Safari") > -1;
-    var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+    // var is_chrome = navigator.userAgent.indexOf('Chrome') > -1;
+    // var is_explorer = navigator.userAgent.indexOf('MSIE') > -1;
+    // var is_firefox = navigator.userAgent.indexOf('Firefox') > -1;
+    // var is_safari = navigator.userAgent.indexOf("Safari") > -1;
+    // var is_opera = navigator.userAgent.toLowerCase().indexOf("op") > -1;
 
-    if ((is_chrome)&&(is_safari)) {is_safari=false;}
-    if ((is_chrome)&&(is_opera)) {is_chrome=false;}
+    // if ((is_chrome)&&(is_safari)) {is_safari=false;}
+    // if ((is_chrome)&&(is_opera)) {is_chrome=false;}
 
-    if (is_safari) {
-        $('main').addClass('temp-browser-is-safari');
-    }
+    // if (is_safari) {
+    //     $('main').addClass('temp-browser-is-safari');
+    // }
 
 
-    $('.temp-browser-is-safari .temp-hide-for-safari').hide();
+    // $('.temp-browser-is-safari .temp-hide-for-safari').hide();
 
 })();
 
